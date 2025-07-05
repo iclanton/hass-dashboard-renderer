026905af9cb3b8030a0d8336cfa600b90015f128
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-07-04 18:32:28 -0700

    Upgrade tooling and Node dependencies.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16-alpine3.17
+FROM node:alpine3.22
 
 WORKDIR /app
 
---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const nodeProfile = require('@rushstack/heft-node-rig/profiles/default/includes/eslint/flat/profile/node-trusted-tool');
+const friendlyLocalsMixin = require('@rushstack/heft-node-rig/profiles/default/includes/eslint/flat/mixins/friendly-locals');
+
+module.exports = [
+  ...nodeProfile,
+  ...friendlyLocalsMixin,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname
+      }
+    }
+  }
+];
